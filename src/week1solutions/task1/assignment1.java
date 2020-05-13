9c91a4f00946dd50042156176668a65f39273fbd
kanishk <kanishkbharadwaj333@gmail.com>
2020-05-13 18:25:15 +0530

    Stack question solved

--- src/week1solutions/task1/assignment1.java
+++ src/week1solutions/task1/assignment1.java
@@ -1,7 +1,38 @@
 package week1solutions.task1;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class assignment1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < string.length() ; i++) {
+            if (string.charAt(i) == '{' || string.charAt(i) == '(' || string.charAt(i) == '['){
+                stack.push(string.charAt(i));
+            }
+
+            if(string.charAt(i) == '}'){
+                if(stack.pop() == '{'){
+                    continue;
+                }else break;
+            }
+
+            if(string.charAt(i) == ']'){
+                if(stack.pop() == '['){
+                    continue;
+                }else break;
+            }
+
+            if(string.charAt(i) == ')'){
+                if(stack.pop() == '('){
+                    continue;
+                }else break;
+            }
+
+        }
+        if(stack.isEmpty()) System.out.println("Yes");
+        else System.out.println("No");
     }
 }
